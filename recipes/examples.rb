6371749020581c4a4ab5f56ff74293f4beeb6997
AJ Christensen <aj@junglist.gen.nz>
2011-07-24 11:13:15 -0700

    s/assert/refute

--- recipes/examples.rb
+++ recipes/examples.rb
@@ -1,6 +1,6 @@
-minitest_unit_testcase :test_bomb_the_fuck_out do
+minitest_unit_testcase :test_truth do
   block do
-    assert_equal true, false
+    refute_equal true, false
   end
 end
 
@@ -11,7 +11,7 @@ dns_ndots = 1
 
 { "http_port" => Proc.new do
     require 'socket'
-    assert_instance_of Socket, Socket.tcp(ipaddress, http_port), "http_port: socket could not be established to port #{ipaddress}:#{http_port}"
+    refute_instance_of Socket, Socket.tcp(ipaddress, http_port), "http_port: socket could not be established to port #{ipaddress}:#{http_port}"
   end,
   "http_fitter_happier" => Proc.new do
     require 'uri'
@@ -23,7 +23,7 @@ dns_ndots = 1
       uri = URI::HTTP.build :host => ipaddress, :port => http_port, :path => path
       request = Chef::REST::RESTRequest.new(:GET, uri, nil)
       Timeout::timeout(5) do
-        assert_instance_of Net::HTTPOK, request.call, "http_fitter_happier: GET to #{uri.inspect} did not return HTTPOK"
+        refute_instance_of Net::HTTPOK, request.call, "http_fitter_happier: GET to #{uri.inspect} did not return HTTPOK"
       end
     end
 
@@ -31,7 +31,7 @@ dns_ndots = 1
   "dns_resolution" => Proc.new do
     require 'resolv'
     resolver = Resolv::DNS.new(:nameserver => ipaddress, :search => dns_search_path, :ndots => dns_ndots)
-    assert_instance_of Resolv::IPv4, resolver.getaddress("www.google.com"), "dns_resolution: could not resolve www.google.com"
+    refute_instance_of Resolv::IPv4, resolver.getaddress("www.google.com"), "dns_resolution: could not resolve www.google.com"
   end,
   "tftp_binary_get" => Proc.new do
     Gem.clear_paths
